112768c9207771b45b3370dd28db24f72ec77936
Richard Birkby <rbirkby@gmail.com>
2014-02-04 21:21:18 +0000

    Add define.printUnresolvedDependencies() for easier debugging

--- ferdinand.js
+++ ferdinand.js
@@ -1,5 +1,5 @@
 /**
- * FerdinandJS - v0.3 - AMD-lite JavaScript module resolver
+ * FerdinandJS - v0.4 - AMD-lite JavaScript module resolver
  *
  * Supports a subset of AMD with the following restrictions:
  *   Static dependencies only, no module loading
@@ -14,18 +14,36 @@
  * Licensed under the MIT License at:
  * 		http://www.opensource.org/licenses/mit-license.php
  *
- * @license MIT License (c) copyright 2013 R Birkby 
+ * @license MIT License (c) copyright 2013-2014 R Birkby 
  */ 
 (function(global) {
     "use strict";
     var cache = {};
     var requireQueue = [];
+
+    function ResolutionError(moduleId) {
+        this.name = 'ResolutionError';
+        this.message = 'Unknown module \'' + moduleId + '\'';
+        this.resolutionChain = [moduleId];
+    }
+
+    ResolutionError.prototype = new Error();
+    ResolutionError.prototype.constructor = ResolutionError;
     
     function resolve(moduleId) {
         var factory = cache[moduleId];
-        if (typeof factory === 'undefined') throw new Error('Unknown module \'' + moduleId + '\'');
+        if (typeof factory === 'undefined') throw new ResolutionError(moduleId);
 
-        factory.__memoized = factory.__memoized || factory.apply(this, factory.__dependencies.map(resolve));
+        try {
+            factory.__memoized = factory.__memoized || factory.apply(this, factory.__dependencies.map(resolve));
+        } catch(e) {
+            if (e instanceof ResolutionError) {
+                e.resolutionChain.push(moduleId);
+            }
+
+            throw e;
+        }
+
         return factory.__memoized;
     }
     
@@ -62,6 +80,34 @@
         return Object.keys(cache)
                      .filter(function(moduleId) {return typeof cache[moduleId].__memoized === 'undefined';});
     };
+    global.define.printUnresolvedDependencies = function() {
+        var dependencies = {};
+        requireQueue.forEach(function(delayedRequire) {
+            delayedRequire.dependencies.forEach(function(moduleId) {
+                dependencies[moduleId] = {};
+            })
+        });
+
+        Object.keys(dependencies).forEach(function(moduleId) {
+            try {
+                resolve(moduleId);
+            } catch(e) {
+                if (e instanceof ResolutionError) {
+                    printResolutionChain(e.resolutionChain.reverse());
+                }
+            }
+        });
+    };
+    function printResolutionChain(chain) {
+        if (chain.length > 1) {
+            console.group('%c' + chain[0], 'color:red');
+            printResolutionChain(chain.slice(1))
+            console.groupEnd();
+        } else {
+            console.log('%c' + chain[0] + '%c remains unresolved', 'color:red', 'color:inherit');
+        }
+    }
+
     global.define.amd = {};
     
     global.require = function(dependencies, callback) {
